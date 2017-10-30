fe7db85d16933b1f8ed6ae9f2c2564732d75d37c
digitopoi <hubo85@gmail.com>
2017-10-30 16:57:21 -0500

    Add project files.

---
+++ PartsInParcels/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PartsInParcels.Startup))]
+namespace PartsInParcels
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
