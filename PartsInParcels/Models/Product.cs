c046945e13dada54b7664f700a9c921fa80dab93
digitopoi <hubo85@gmail.com>
2017-10-30 17:33:17 -0500

    Add Product model

---
+++ PartsInParcels/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PartsInParcels.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string Grade { get; set; }
+        public bool Cured { get; set; }
+        public string Origin { get; set; }
+        public int Weight { get; set; }
+    }
+}
